e76faf39d0025702368fe99f4c3d45ffc20f49fc
nguyenphanthaiphong <95667071+nguyenphanthaiphong@users.noreply.github.com>
2022-08-20 00:02:57 +0700

    Add files via upload

---
+++ js09.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var arr=[];
+    $("ol>li").click(function(){
+        $("p#view").text($(this).text())
+    });
+    $("#no").click(function(){
+        $("#view").removeClass("shadow");
+    });
+    $("#yes").click(function(){
+        $("#view").addClass("shadow");
+    });
+    $("#textcolor").change(function(){
+        $("#view").css("color",$(this).val());
+    });
+    $("#btDel").click(function(){
+        var text=$("ol li:last").text();
+        if(text=="") return;
+        arr.unshift(text);
+        $("ol li:last").remove();
+    });
+    $("#btUndo").click(function(){
+        if(arr.length==0) return;
+        var item="<li>"+arr.shift()+"</li>";
+        $("ol").appen(item);
+    });
+})
